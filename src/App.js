5b9faea9b714949e2aaa9aefcad90bc69181c570
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-11-09 15:09:03 +0000

    Got all components
    All components are rendered. Need to style it better.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,48 @@ const HeaderText = (props) => {
   <div>
     <p className='header-name'>{props.name}</p>
     <p className='header-text'>{props.text}</p>
+    <img className='img' src='me.jpg' />
   </div>
   )
 }
 
+const myCv = {
+  name: 'Kristján Sigurðsson',
+  text: 'Þjónustufulltrúi, Nemi, Knattspyrnuáhugamaður',
+  jobs: [
+    {
+      name: 'Samskip HF',
+      time: '2020 enn að.',
+      text: 'Þjónustufulltrúi, sé um samskipti við viðskiptavini, skráningar í birgðakerfi ásamt tilfallandi verkefnum.'
+    },
+    {
+      name: 'Samskip HF',
+      time: '2014 - 2020',
+      text: 'Lyftaramaður, vann við lestun og losun frystigáma og fráganf inn í frystiklefa í frystigeymslu Samskipa.'
+  },
+  {
+    name: 'Vinnslustöð Vestmannaeyja',
+    time: '2010 - 2014',
+    text: 'Lyftaramaður, sá um pökkun og frágang frystivöru og skráningar í birgðir í fristiklefa.'
+}
+  ]
+}
+
+
 const App = () => {
-  const name = 'Kristján Sigurðsson'
-  const text = 'Þjónustufulltrúi, Nemandi, Knattspyrnuahugamaður'
   return (
     <div className="App">
       <header className="App-header">
-        <HeaderText name={name} text={text} />
+        <HeaderText className='header' name={myCv.name} text={myCv.text} />
+        <p className='job-name'>{myCv.jobs[0].name}</p>
+        <p className='job-time'>{myCv.jobs[0].time}</p>
+        <p className='job-text'>{myCv.jobs[0].text}</p>
+        <p className='job-name'>{myCv.jobs[1].name}</p>
+        <p className='job-time'>{myCv.jobs[1].time}</p>
+        <p className='job-text'>{myCv.jobs[1].text}</p>
+        <p className='job-name'>{myCv.jobs[2].name}</p>
+        <p className='job-time'>{myCv.jobs[2].time}</p>
+        <p className='job-text'>{myCv.jobs[2].text}</p>
       </header>
     </div>
   );
